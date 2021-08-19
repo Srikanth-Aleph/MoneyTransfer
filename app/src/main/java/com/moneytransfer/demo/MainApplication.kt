b3bbc4f8d8263987e118f5d7633200a73a35af27
sayfullah <sayfullah@aleph-labs.com>
2021-08-19 10:44:58 +0800

    -Pulled from master branch - rectified custom-http issue - added Koin dependency

--- app/src/main/java/com/moneytransfer/demo/MainApplication.kt
+++ app/src/main/java/com/moneytransfer/demo/MainApplication.kt
@@ -1,8 +1,6 @@
 package com.moneytransfer.demo
 
 import android.app.Application
-import org.koin.android.BuildConfig
-import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import timber.log.Timber
 
@@ -19,12 +17,7 @@ class MainApplication : Application() {
         if(BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
 
         startKoin {
-            androidContext(this@MainApplication)
-            modules(
-                listOf(
 
-                )
-            )
         }
 
 
