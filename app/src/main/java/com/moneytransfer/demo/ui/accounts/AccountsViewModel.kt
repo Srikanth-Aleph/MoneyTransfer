585c6fb61b5a406f8c69cd3f8715d5f64e2ebb15
Srikanth Kancheti <srikanth@aleph-labs.com>
2021-08-19 15:41:35 +0800

    removed wrong dependency

--- app/src/main/java/com/moneytransfer/demo/ui/accounts/AccountsViewModel.kt
+++ app/src/main/java/com/moneytransfer/demo/ui/accounts/AccountsViewModel.kt
@@ -3,12 +3,12 @@ package com.moneytransfer.demo.ui.accounts
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.moneytransfer.core.ktor.KtorMockService
+import com.moneytransfer.accounts.AccountsFeature
 
 class AccountsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = KtorMockService.getDummyResponse().toString()
+        value = AccountsFeature.getFromAccountsModule()
     }
     val text: LiveData<String> = _text
 }
