ef724d2d67482bdf07035de780b06cda2673266a
Srikanth Kancheti <88026630+Srikanth-Aleph@users.noreply.github.com>
2021-08-23 19:59:10 +0800

    Added Ktor post API to show accounts list (#9)
    * local copy
    
    * - assed post ktor api to show accounts list

--- accounts/src/main/java/com/moneytransfer/accounts/model/AccountResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.moneytransfer.accounts.model
-
-import com.google.gson.annotations.SerializedName
-
-data class AccountResponse(
-    @SerializedName("payload")
-    val payLoad: Account
-)
---
+++ accounts/src/main/java/com/moneytransfer/accounts/model/response/AccountResponse.kt
@@ -0,0 +1,31 @@
+package com.moneytransfer.accounts.model.response
+
+import com.google.gson.annotations.SerializedName
+
+data class AccountsResponse(
+    @SerializedName("payload")
+    val payLoad: AccountsPayload
+)
+
+data class AccountsPayload(
+    @SerializedName("responseId")
+    val id: String,
+    @SerializedName("irrelevantMetaData")
+    val irrelevantData: Int,
+    @SerializedName("anotherIrrelevantMetaData")
+    val anotherIrrelevantMetaData: String,
+    @SerializedName("accounts")
+    val accounts: List<AccountItem>
+
+)
+
+data class AccountItem(
+    @SerializedName("id")
+    val id: String,
+    @SerializedName("accountNumber")
+    val accountNumber: String,
+    @SerializedName("accountName")
+    val accountName: String,
+    @SerializedName("availableBalance")
+    val availableBalance: String,
+)
